bf3a9136311056f543473bc870060118e7271339
BerryGoodies <berkeguducu@gmail.com>
2017-09-04 17:32:04 +0300

    changed theme, added extra php, css and js

---
+++ script-n-styles.js
@@ -0,0 +1,11 @@
+//allowfullscreen for iframe(youtube)
+n = -1
+while (n < 50) {
+    n = n + 1;
+    var iframe = document.getElementsByTagName("iframe")[n];   // Get the [n]th <iframe> element in the document
+    var att = document.createAttribute("allowfullscreen");       // Create a "allowfullscreen" attribute
+    att.value = "1";                           // Set the value of the class attribute
+    iframe.setAttributeNode(att);
+}
+
+document.getElementsByTagName("body").className += " MyClass";
